70a11588b9e4615754f6a56c8f9f98abbfb77736
TinaSayapina <tina.sayapina@toptal.com>
2024-01-25 18:22:27 +0500

    Filesystem

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises'
+
 const create = async () => {
-    // Write your code here 
+    const filepath = "./src/fs/files/fresh.txt"
+    const content = 'I am fresh and young';
+    try {
+        await fs.writeFile(filepath, content, {flag: 'ax+'});
+    } catch (e) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await create();
