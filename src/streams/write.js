055ae3050f8e9bdc341ef3757e5cba67a4766604
TinaSayapina <tina.sayapina@toptal.com>
2024-01-26 18:43:00 +0500

    Streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import fs from 'fs'
+
+const dir = import.meta.dirname;
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(`${dir}/files/fileToWrite.txt`)
+    process.stdin.on('data', chunk => {
+        writeStream.write(chunk)
+    })
 };
 
 await write();
