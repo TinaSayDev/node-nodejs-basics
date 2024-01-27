3c774d469dad2aecea92b6c5fdf60a7587d9c574
TinaSayapina <tina.sayapina@toptal.com>
2024-01-27 17:29:03 +0500

    Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import {createGzip} from 'node:zlib'
+import {pipeline} from 'node:stream'
+import {createReadStream, createWriteStream} from 'node:fs'
+
+const dir = import.meta.dirname;
+
 const compress = async () => {
-    // Write your code here 
+
+    const gzip = createGzip();
+    const source = createReadStream(`${dir}/files/fileToCompress.txt`);
+    const destination = createWriteStream(`${dir}/files/archive.gz`);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
